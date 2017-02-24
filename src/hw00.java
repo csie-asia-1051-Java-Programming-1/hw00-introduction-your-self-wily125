d8a09b86b460ed53b08ff0bf76c2b94a83217896
wily125 <105021024@live.asia.edu.tw>
2017-02-24 14:59:23 +0800

    hw00 submit

---
+++ src/hw00.java
@@ -0,0 +1,13 @@
+/*
+* Topic: 自我介紹
+* Date: 2017/02/24
+* Author: 105021024
+*/
+public class hw00 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("學系:資工系1A資電組\n 學號:105021024\f\n 姓名:任韋丞");		
+	}
+
+}
